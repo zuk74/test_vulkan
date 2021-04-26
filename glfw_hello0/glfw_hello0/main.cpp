11299e312ec4509abf1a2bb64ecd96abfc89a40e
Shingo.Zukunashi <shingo.zukunashi@gmail.com>
2021-04-27 00:00:46 +0900

    first commit.

---
+++ glfw_hello0/glfw_hello0/main.cpp
@@ -0,0 +1,18 @@
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
